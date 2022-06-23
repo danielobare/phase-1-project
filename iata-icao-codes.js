a3ba7f63df19cd356ee6c254d8e0f1f01d6ee71b
Daniel Obare N <dnobare@gmail.com>
2022-06-23 20:27:02 +0300

    Made styling and structural changes to html & css. Also added some images

---
+++ iata-icao-codes.js
@@ -0,0 +1,15 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '68cf647b7amsh2469ec3887468a1p18243ajsncf1aad177e7f',
+		'X-RapidAPI-Host': 'iata-and-icao-codes.p.rapidapi.com'
+	}
+};
+
+fetch('https://iata-and-icao-codes.p.rapidapi.com/airlines', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+//free
+//works
