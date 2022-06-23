2dc22d7ae14a1faedd8e6b8b5df48ed288f4f025
Daniel Obare N <dnobare@gmail.com>
2022-06-23 16:15:12 +0300

    Changed google-flight-search code to JavaScript fetch

--- Js/google-flight-search.js
+++ Js/google-flight-search.js
@@ -1,24 +1,15 @@
-const unirest = require("unirest");
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '68cf647b7amsh2469ec3887468a1p18243ajsncf1aad177e7f',
+		'X-RapidAPI-Host': 'google-flights-search.p.rapidapi.com'
+	}
+};
 
-const req = unirest("GET", "https://google-flights-search.p.rapidapi.com/search");
-
-req.query({
-	"departure_airport_code": "SFO",
-	"arrival_airport_code": "LAX",
-	"flight_class": "Economy"
-});
-
-req.headers({
-	"X-RapidAPI-Key": "68cf647b7amsh2469ec3887468a1p18243ajsncf1aad177e7f",
-	"X-RapidAPI-Host": "google-flights-search.p.rapidapi.com",
-	"useQueryString": true
-});
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
-});
+fetch('https://google-flights-search.p.rapidapi.com/search?departure_airport_code=SFO&arrival_airport_code=LAX&flight_class=Economy', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
 
 //freemium
 
